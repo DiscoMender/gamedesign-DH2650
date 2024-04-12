f9dd0b2dceac0248d75944953d34132126190eaa
Juan Lavagnini <juanlavagnini@gmail.com>
2024-04-12 11:44:19 +0200

    Lose by time detection

--- Assets/Scenes/FindTheKey/hasWon.cs
+++ Assets/Scenes/FindTheKey/hasWon.cs
@@ -7,6 +7,16 @@ public class hasWon : MonoBehaviour
     public bool haswon = false;
     public bool key = false;
 
+    [SerializeField]
+    private Timer timer;
+
+    void Update()
+    {
+        if(timer.remainingSeconds==0f)
+        {
+            PlayerStats.LoseMinigame("FindTheKey");
+        }    
+    }
     void OnCollisionEnter2D(Collision2D collisionInfo)
     {
         if (collisionInfo.collider.name == "doorSquarePosition")
