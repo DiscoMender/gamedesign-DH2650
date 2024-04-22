68ebd9425c3d665f55086c82384f93bdec6ab31f
Juan Lavagnini <juanlavagnini@gmail.com>
2024-04-22 14:52:38 +0200

    update of game

--- Assets/Scenes/RoadWithCars/RoadWithCarshasWon.cs
+++ Assets/Scenes/RoadWithCars/RoadWithCarshasWon.cs
@@ -2,22 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RoadWithBoxesHasWon : MonoBehaviour
+public class RoadWithCarsHasWon : MonoBehaviour
 {
     [SerializeField] private Timer timer;
     void Update()
     {
         if (timer.remainingSeconds == 0f)
         {
-            PlayerStats.WinMinigame("RoadWithBoxes");
+            PlayerStats.WinMinigame("RoadWithCars");
         }
     }
 
     void OnCollisionEnter2D(Collision2D collisionInfo)
     {
-        if (collisionInfo.collider.name == "box(Clone)")
+        if (collisionInfo.collider.name == "car(Clone)")
         {
-            PlayerStats.LoseMinigame("RoadWithBoxes");
+            PlayerStats.LoseMinigame("RoadWithCars");
         } 
     }
 }
