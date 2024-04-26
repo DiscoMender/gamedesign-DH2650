c6f12c0db5b5caf96500e54e29aadbc2ecfa36c3
MercuriaL01 <iley.alvarez.funcke@gmail.com>
2024-04-26 10:56:07 +0200

    added coin and tuby counter
    coin and ruby counter works as intended

---
+++ Assets/Scenes/Coins/scripts/CoinCounter.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CoinCounter : MonoBehaviour
+{
+    public static CoinCounter instance;
+
+    [SerializeField] private TMP_Text coinText;
+    [SerializeField] private int currentCoins = 0;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinText.text = "COINS: " + currentCoins.ToString() + " / 15";
+    }
+
+    public void IncreaseCoins(int v)
+    {
+        currentCoins += v;
+        coinText.text = "COINS: " + currentCoins.ToString() + " / 15";
+    }
+}
