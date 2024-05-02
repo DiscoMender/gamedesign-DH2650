da5bd5a11caeddcb62722d654272bab7c43c6961
Charles0510 <ccharleskou@gmail.com>
2024-05-02 22:42:28 +0200

    Introduction
    Two characters introduce themselves.
    
    Done: Azureus run till position
    
    TODO: Add textbox, tell the story, and add green one in to it

---
+++ Assets/Scenes/Introduction/AzureusIntro.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AzureusIntro : MonoBehaviour
+{
+    private Animator animator;
+    public float speed = 5f;
+    public Vector3 startPosition = new Vector3(-12f, -0.52f, 0);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // initialize
+        animator = GetComponent<Animator>();
+
+        // set start position
+        transform.position = startPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // run until reach the desired position(x = -3,y = -0.52)
+        // run animator is set automatically at the start
+        
+        transform.position += Vector3.right * speed * Time.deltaTime;
+
+        Debug.Log(transform.position.x);
+
+        // stay idle
+        if (transform.position.x >= -3f)
+        {
+            // play idle animation
+            animator.SetTrigger("Idle");
+
+            enabled = false;
+        }
+    }
+}
